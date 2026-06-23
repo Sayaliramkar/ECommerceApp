dbae060fa02913da89b8dc232e573b7875bc47ec
Sayaliramkar <ramkarsayali0@gmail.com>
2026-06-23 17:46:14 +0530

    Added search bar functionality

---
+++ Frontend/src/components/SearchBar.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import { assets } from '../assets/assets';
+import { useLocation } from 'react-router-dom';
+
+const SearchBar = () => {
+
+    const { serach, setSearch, showSearch, setShowSearch } = useContext(ShopContext);
+    const [visible, setVisible] = useState(false);
+    const location = useLocation();
+
+
+
+    useEffect(() => {
+        if (location.pathname.includes('collection')) {
+            setVisible(true);
+        }
+        else {
+            setVisible(false);
+        }
+    }, [location])
+
+    return showSearch ? (
+        <div className='border-t border-b bg-gray-50 text-center'>
+            <div className='inline-flex item-center justify-center border border-gray-400 px-5 py-2 mx-3 my-5 rounded-full w-3/4 sm:w-1/2'>
+                <input value={serach} onChange={(e) => setSearch(e.target.value)} className='flex-1 outline-none bg-inherit text:sm' />
+                <img src={assets.search_icon} className='w-4' />
+            </div>
+            <img onClick={() => setShowSearch(false)} src={assets.cross_icon} className='inline w-3 cursor-pointer' />
+
+
+        </div>
+    ) : null
+}
+
+export default SearchBar
--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,19 +1,22 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState} from 'react';
 import { products } from '../assets/assets';
 
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
-    const currecy = '$';
+    const currency = '$';
     const delivery_fee = 10;
-
+    const [search, setSearch] = useState('');
+    const[showSearch, setShowSearch] = useState(false);
     
     const value = {
-          products,
-          currecy,
-          delivery_fee
+      products, currency, delivery_fee,
+      search,setSearch,showSearch,setShowSearch
     }
+
+    
+   
 
     return(
        < ShopContext.Provider value={value}>
