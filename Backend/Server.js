55df7744b953259cd9c2954a5ce828b92f51cdf2
Sayaliramkar <ramkarsayali0@gmail.com>
2026-06-30 13:08:00 +0530

    Added registration property

--- Backend/Server.js
+++ Backend/Server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import { connect } from 'mongoose'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
 
 //App config
 const app = express()
@@ -16,6 +17,7 @@ app.use(express.json())
 app.use(cors())
 
 //api endpoints
+app.use('/api/user',userRouter)
 app.get('/',(req,res)=>{
      res.send("Api working")
 })
