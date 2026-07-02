56d51076dea69af4e9ca23aecafca43f43eee694
Sayaliramkar <ramkarsayali0@gmail.com>
2026-07-02 16:56:47 +0530

    AddProduct feature added

--- Backend/Server.js
+++ Backend/Server.js
@@ -5,6 +5,7 @@ import { connect } from 'mongoose'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
 
 //App config
 const app = express()
@@ -18,6 +19,8 @@ app.use(cors())
 
 //api endpoints
 app.use('/api/user',userRouter)
+app.use('/api/product',productRouter)
+
 app.get('/',(req,res)=>{
      res.send("Api working")
 })
--- Backend/config/cloudinary.js
+++ Backend/config/cloudinary.js
@@ -1,12 +1,20 @@
-import {v2 as cloudinary} from "cloudinary"
+import { v2 as cloudinary } from "cloudinary";
 
 const connectCloudinary = async () => {
 
+    console.log({
+        cloud: process.env.CLOUDINARY_NAME,
+        key: process.env.CLOUDINARY_API_KEY,
+        secret: process.env.CLOUDINARY_SECRET_KEY
+    });
+
     cloudinary.config({
-        cloud_name : process.env.CLOUDINARY_NAME,
-        api_key : process.env.CLOUDINAR_API_KEY,
-        api_secret: process.env.CLOUDINARY_SECRET_KEY
-    })
-}
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET_KEY,
+    });
+
+    console.log(cloudinary.config());
+};
 
 export default connectCloudinary;
